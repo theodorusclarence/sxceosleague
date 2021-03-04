9877b4004174470c1daacc274de85844d832460a
Theodorus Clarence <theodorusclarence@gmail.com>
2021-03-04 22:42:08 +0700

    Update: swapped homepage to index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,160 +1,95 @@
-import ButtonLink from '@/components/ButtonLink';
-import { NextSeo } from 'next-seo';
 import Image from 'next/image';
+import { NextSeo } from 'next-seo';
+import Nav from '@/components/Nav';
+import ButtonLink from '@/components/ButtonLink';
+import EventLists from '@/components/EventLists';
+import Footer from '@/components/Footer';
 
-export default function Home() {
+export default function HomePage() {
     return (
         <>
             <NextSeo />
-            <section className=''>
-                <div className='grid h-screen overflow-hidden text-white parent'>
-                    <main className=' bg-gradient-to-br from-spurple-500 to-spurple-600'>
-                        <div className='relative z-10 flex items-center justify-center h-full layout-left'>
-                            <article className='z-10 flex flex-col items-start mx-auto md:transform md:translate-x-16 lg:translate-x-28'>
-                                <h1 className='text-6xl md:text-8xl'>
-                                    Coming Soon
-                                </h1>
-                                <h3>StudentsxCEOs League</h3>
-                                <ButtonLink
-                                    href='/staff-enrollment'
-                                    className='mt-4'
-                                >
-                                    Register Staff
-                                </ButtonLink>
-                            </article>
-                            <figure className='absolute select-none -bottom-12 left-8 w-44 md:w-72 md:left-auto md:-right-32 animate-float'>
-                                <Image
-                                    priority
-                                    width={997}
-                                    height={1000}
-                                    layout='responsive'
-                                    src='/images/blur-triangle.png'
-                                    alt='illustration'
-                                />
-                            </figure>
-                            <figure className='absolute right-0 select-none bottom-12 md:hidden w-44 animate-float float-delay-2'>
-                                <Image
-                                    priority
-                                    width={261}
-                                    height={366}
-                                    layout='responsive'
-                                    src='/images/blur-triangle-blue.png'
-                                    alt='illustration'
-                                />
-                            </figure>
-                            <figure className='absolute w-32 select-none top-12 -right-12 md:-right-20 md:w-52 '>
-                                <Image
-                                    priority
-                                    width={534}
-                                    height={366}
-                                    layout='responsive'
-                                    src='/images/quarter-circle.png'
-                                    alt='illustration'
-                                />
-                            </figure>
-                            <figure className='absolute top-0 hidden select-none md:block md:w-32 left-48'>
-                                <Image
-                                    priority
-                                    width={306}
-                                    height={1128}
-                                    layout='responsive'
-                                    src='/images/long-illustration-2.png'
-                                    alt='illustration'
-                                />
-                            </figure>
-                            <figure className='absolute z-0 select-none -left-12 w-60 float-delay top-1/4 md:top-auto md:bottom-28 md:left-4 md:w-96 animate-float'>
-                                <Image
-                                    priority
-                                    width={1026}
-                                    height={1026}
-                                    layout='responsive'
-                                    src='/images/blur-circle.png'
-                                    alt='illustration'
-                                />
-                            </figure>
-                            <div className='absolute bottom-0 z-20 grid grid-cols-3 gap-2 px-8 py-2 transform translate-y-1/2 bg-white rounded-full social md:gap-4 md:py-12 md:px-6 md:grid-cols-1 md:grid-rows-3 md:right-0 md:bottom-auto md:top-1/2 md:-translate-y-1/2 md:translate-x-1/2 '>
-                                <a
-                                    rel='noopener noreferer'
-                                    target='_blank'
-                                    href='#'
-                                >
-                                    <img
-                                        className='w-8 h-8 md:w-10 md:h-10'
-                                        src='/images/logo-ig.svg'
-                                    />
-                                </a>
-                                <a
-                                    rel='noopener noreferer'
-                                    target='_blank'
-                                    href='#'
-                                >
-                                    <img
-                                        className='w-8 h-8 md:w-10 md:h-10'
-                                        src='/images/logo-linkedin.svg'
-                                    />
-                                </a>
-                                <a
-                                    rel='noopener noreferer'
-                                    target='_blank'
-                                    href='#'
-                                >
-                                    <img
-                                        className='w-8 h-8 md:w-10 md:h-10'
-                                        src='/images/logo-mail.svg'
-                                    />
-                                </a>
-                            </div>
+            <Nav />
+            <main>
+                <section className='relative bg-gradient-to-r from-spurple-500 to-spurple-600'>
+                    <figure className='absolute z-0 w-12 pointer-events-none top-6 right-16 md:right-64 md:w-20 lg:right-96'>
+                        <Image
+                            src='/images/long-illustration-2.png'
+                            alt='illustration'
+                            width={200}
+                            height={800}
+                            priority
+                            loading='eager'
+                            layout='responsive'
+                        />
+                    </figure>
+                    <figure className='absolute w-32 pointer-events-none right-12 top-24 animate-float md:top-52 md:right-52 md:w-48 lg:right-80'>
+                        <Image
+                            priority
+                            loading='eager'
+                            width={182}
+                            height={181}
+                            layout='responsive'
+                            src='/images/blur-square.png'
+                            alt='illustration'
+                        />
+                    </figure>
+                    <figure
+                        style={{ transform: 'translateY(42.9%)' }}
+                        className='absolute bottom-0 right-0 z-0 pointer-events-none w-60 md:w-96 '
+                    >
+                        <Image
+                            src='/images/quartet-illus.png'
+                            width={424}
+                            height={742}
+                            priority
+                            loading='eager'
+                            layout='responsive'
+                        />
+                    </figure>
+                    <article className='relative flex flex-col justify-center min-h-screen space-y-4 text-white layout'>
+                        <figure className='w-12 md:w-20'>
+                            <Image
+                                src='/images/logo/logo.png'
+                                width={77}
+                                height={113}
+                                priority
+                                loading='eager'
+                                layout='responsive'
+                                alt='Logo'
+                            />
+                        </figure>
+                        <h1 className='text-4xl md:text-6xl'>
+                            StudentsxCEOs
+                            <br />
+                            League
+                        </h1>
+                        <p className='max-w-xl leading-7 md:text-lg'>
+                            The biggest annual event held by StudentsxCEOs East
+                            Java, which is aimed to become a Development
+                            Platform.
+                        </p>
+                        <ButtonLink className='self-start' href='/register'>
+                            Register Now
+                        </ButtonLink>
+                    </article>
+                </section>
+                <section className='bg-beige'>
+                    <article className='relative py-24 pt-36 layout'>
+                        <h2 className='text-center'>
+                            What's On{' '}
+                            <span className='block md:inline-block'>
+                                StudentsxCEOs League
+                            </span>
+                        </h2>
+                        <hr className='h-2 mx-auto mt-2 border-0 rounded w-60 md:w-80 bg-gradient-to-r from-sgreen-500 to bg-sgreen-600' />
+                        <div className='mt-12'>
+                            <EventLists />
                         </div>
-                    </main>
-                    <aside className='bg-gradient-to-b md:bg-gradient-to-r from-sgreen-500 to-sgreen-600'>
-                        <div className='relative flex flex-col items-center justify-end h-full layout-right'>
-                            <figure className='w-8 select-none md:w-28'>
-                                <Image
-                                    priority
-                                    width={460}
-                                    height={1132}
-                                    layout='responsive'
-                                    src='/images/long-illustration.png'
-                                    alt='illustration'
-                                />
-                            </figure>
-                        </div>
-                    </aside>
-                </div>
-                <style jsx>{`
-                    article {
-                        max-width: 13rem;
-                    }
-                    .parent {
-                        grid-template-rows: 80% 20%;
-                    }
-
-                    .social {
-                        box-shadow: 17px 25px 95px rgba(85, 56, 143, 0.35);
-                    }
-
-                    @media (min-width: 768px) {
-                        .parent {
-                            grid-template-rows: auto;
-                            grid-template-columns: 70% 30%;
-                        }
-
-                        article {
-                            max-width: 20rem;
-                        }
-
-                        .layout-left {
-                            max-width: 1000px;
-                            margin-left: auto;
-                        }
-
-                        .layout-right {
-                            max-width: 1430px;
-                            margin-right: auto;
-                        }
-                    }
-                `}</style>
-            </section>
+                    </article>
+                </section>
+            </main>
+            <Footer />
         </>
     );
 }
