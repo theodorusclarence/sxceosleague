125d9e9c81987b2214d5da5bc78525ca7589586b
Theodorus Clarence <theodorusclarence@gmail.com>
2021-03-05 19:59:22 +0700

    Update: change excerpt for IBL

--- data/data.js
+++ data/data.js
@@ -17,7 +17,7 @@ export const eventsData = [
         id: 3,
         title: 'Conference',
         excerpt:
-            'An adapted version of contributing and sharing your potential idea in policy-making recommendation with students all over Indonesia. A deeper analysis packed in a fruitful discussion with expert will be facilitated in this conference and participants will get a bigger chance to enhance their knowledge.',
+            'Brace yourself to meet your peers with various perspectives and buckle up, as we immerse into the business world and work through its problem in digitalization. Be prepared to widen your breadth of view and come out brighter than before.',
         route: '/conference',
     },
 ];
