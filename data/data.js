7c053f85b65e94dc4c2a76883204a9e95b2a0abc
Theodorus Clarence <theodorusclarence@gmail.com>
2021-03-06 13:12:31 +0700

    Update: data update

--- data/data.js
+++ data/data.js
@@ -10,14 +10,14 @@ export const eventsData = [
         id: 2,
         title: 'IBL Case Challenge',
         excerpt:
-            'The Innovative Business Leaders Case Challenge is a case based competition that will encourage each participant to see himself as a leader, who is responsible for solving a business problem of a company in a creative and innovative way with a high level of realization and practice.',
+            'Brace yourself to meet your peers with various perspectives and buckle up, as we immerse into the business world and work through its problem in digitalization. Be prepared to widen your breadth of view and come out brighter than before.',
         route: '/ibl-case-challenge',
     },
     {
         id: 3,
         title: 'Conference',
         excerpt:
-            'Brace yourself to meet your peers with various perspectives and buckle up, as we immerse into the business world and work through its problem in digitalization. Be prepared to widen your breadth of view and come out brighter than before.',
+            'An adapted version  of contributing and sharing your potential idea in policy-making recommendation with students all over Indonesia. A deeper analysis packed in a fruitful discussion with expert will be facilitated in this conference and participants will get a bigger chance to enhance their knowledge.',
         route: '/conference',
     },
 ];
