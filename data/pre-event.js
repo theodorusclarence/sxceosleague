369be775ea4ecca69d0837f3a1848cfc5b0d8d35
Theodorus Clarence <theodorusclarence@gmail.com>
2021-04-28 21:14:39 +0700

    feat(pre-event): add tabs

---
+++ data/pre-event.js
@@ -0,0 +1,26 @@
+export const tabData = [
+    {
+        name: 'Talks and Discussion Session',
+        time: '27 May',
+        desc:
+            'A session where audiences will gain knowledge from our speakers through an interactive discussion platform to prepare youth for the professional career world.',
+        buttonText: 'Coming Soon!',
+        href: null,
+    },
+    {
+        name: 'Mini Challenge',
+        time: '2 - 15 May',
+        desc:
+            'A mini case challenge to challenge your complex problem solving skills and to warm you up before the IBL Case Competition. Winners of this challenge will get some interesting prizes!',
+        buttonText: 'Submit for the challenge',
+        href: '/',
+    },
+    {
+        name: 'Instagram Live Session',
+        time: '7 May & 18 June',
+        desc:
+            'A discussion session between us and interesting speakers through the Instagram Live feature. Get ready to hear the other side of every story!',
+        buttonText: 'Coming Soon!',
+        href: null,
+    },
+];
