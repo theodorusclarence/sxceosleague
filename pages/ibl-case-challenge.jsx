b172ce23ffa0e7d1462167f17a01141eaf49cdb0
reyhannaufal <reyhannaufal21@gmail.com>
2021-05-04 21:32:58 +0700

    feat(ibl-case): case soon to be announced

--- pages/ibl-case-challenge.jsx
+++ pages/ibl-case-challenge.jsx
@@ -1,22 +1,19 @@
-import Image from 'next/image';
-import { NextSeo } from 'next-seo';
-import Nav from '@/components/Nav';
-
-import EventLists from '@/components/EventLists';
-import Footer from '@/components/Footer';
-import ScrollDownArrow from '@/components/ScrollDownArrow';
 import ButtonLink from '@/components/ButtonLink';
-import { rotate } from 'tailwindcss/defaultTheme';
+import Footer from '@/components/Footer';
+import Nav from '@/components/Nav';
 import PurpleLabel from '@/components/PurpleLabel';
+import ScrollDownArrow from '@/components/ScrollDownArrow';
+import { NextSeo } from 'next-seo';
+import Image from 'next/image';
 
 const title = 'IBL Case Challenge - StudentsxCEOs League';
 const description =
     'A series of events ranging from challenges to an in-depth discussion session to warm you up before participating in our main events.';
 const url = 'https://sxceosleague.com/ibl-case-challenge';
 
-export default function ibl_case_challenge() {
+export default function Conference() {
     return (
-        <>
+        <div className='relative overflow-x-hidden'>
             <NextSeo
                 title={title}
                 description={description}
@@ -28,77 +25,58 @@ export default function ibl_case_challenge() {
                 }}
             />
             <Nav />
-            <main className='overflow-x-hidden'>
+            <main>
+                {/* HERO SECTION */}
                 <section
-                    className='relative bg-gradient-to-r from-spurple-500 to-spurple-600'
+                    className='relative'
                     style={{ background: 'rgba(238, 252, 242, 1)' }}
                 >
-                    {/* ibl-cc-logo */}
-                    <figure className='absolute z-20 invisible w-32 transform translate-y-20 pointer-events-none md:z-30 translate-x-44 lg:visible right-12 md:top-52 md:right-72 md:w-48 lg:right-80'>
+                    <figure className='select-none pointer-events-none absolute w-120 left-1/4 -bottom-40 md:left-1/2 md:w-[1200px] md:bottom-auto md:top-1/2 transform md:-translate-y-1/2 md:pl-10 md:pt-80'>
+                        {/* Triangle Illustration */}
                         <Image
-                            priority
-                            loading='eager'
-                            width={305}
-                            height={690}
-                            layout='responsive'
-                            src='/images/ibl-cc-logo.png'
-                            alt='illustration'
-                        />
-                    </figure>
-                    {/*small rectangle */}
-                    <figure className='absolute bottom-0 right-0 z-10 w-64 transform translate-y-10 pointer-events-none lg:w-120 lg:z-20 lg:-translate-y-36 lg:right-5'>
-                        <Image
-                            src='/images/Rectangle-28.png'
-                            width={360}
-                            height={400}
+                            src='/images/circle-group.png'
+                            width={1000}
+                            height={850}
                             priority
                             loading='eager'
                             layout='responsive'
+                            alt='rectangle Illustration'
                         />
                     </figure>
-                    {/* large rectangle */}
-                    <figure
-                        style={{ transform: 'translateY(42.9%)' }}
-                        className='absolute bottom-0 right-0 transform pointer-events-none lg:z-10 lg:translate-x-120 lg:bottom-40 w-60 lg:w-120 '
-                    >
-                        <Image
-                            src='/images/Rectangle-29.png'
-                            width={424}
-                            height={742}
-                            priority
-                            loading='eager'
-                            layout='responsive'
-                        />
-                    </figure>
-                    <article className='relative flex flex-col justify-center min-h-screen space-y-4 text-white layout'>
-                        {/* Logo StudentsxCEOs League */}
-                        <div className='max-w-xl space-y-5'>
+                    <article className='relative flex flex-col items-start justify-center min-h-screen text-black md:items-center md:justify-between md:flex-row-reverse layout'>
+                        {/* Add spacing when stacking vertically */}
+                        <figure className='w-16 mb-8 md:w-64 md:mb-0'>
+                            {/* Logo */}
+                            <Image
+                                src='/images/ibl-cc-logo.png'
+                                width={216}
+                                height={485}
+                                priority
+                                loading='eager'
+                                layout='responsive'
+                                alt='Logo-Conference'
+                            />
+                        </figure>
+                        <div className='max-w-xl space-y-4'>
                             <PurpleLabel>StudentsxCEOs League</PurpleLabel>
-
-                            <h1 className='text-4xl text-spurple-400 md:text-5xl'>
+                            <h1 className='text-5xl text-spurple-400 md:text-7xl'>
                                 IBL Case Challenge
                             </h1>
-                            <p className='leading-6 text-gray-900 md:leading-8 md:text-lg'>
+                            <p className='leading-6 text-gray-900 md:leading-8'>
                                 Case based competition that will encourage each
                                 participant to see themself as a leader, who is
                                 responsible for solving a business problem of a
                                 company in a creative and innovative way with a
                                 high level of realization and practice.
                             </p>
+                            {/* <ButtonLink href='/' className='inline-block'>
+                                Button
+                            </ButtonLink> */}
                         </div>
-
-                        {/* <ButtonLink
-                            className='self-start'
-                            href='https://tiny.cc/RegistrationTnD'
-                        >
-                            Register Talks and Discussion
-                        </ButtonLink> */}
-                        {/* Hide on mobile */}
-                        <figure className='hidden md:block'>
-                            <ScrollDownArrow colorClass='text-white' />
-                        </figure>
+                        <ScrollDownArrow />
                     </article>
                 </section>
+                {/* JP MORGAN Section */}
                 <section
                     style={{
                         background:
@@ -106,10 +84,22 @@ export default function ibl_case_challenge() {
                     }}
                 >
                     <article className='relative py-24 md:pt-36 layout'>
-                        <h2 className='mb-10 text-center text-white md:mb-20'>
+                        <h2 className='mb-10 text-center text-white'>
                             Case Contributor
                         </h2>
-                        <div className='relative px-10 py-20 space-x-3 sm:py-28 md:py-40 md:space-y-5 rounded-2xl bg-beige lg:mt-52'>
+
+                        <h1
+                            style={{
+                                background:
+                                    'linear-gradient(90deg, #F44E61 0%, #FA8A97 100%)',
+                            }}
+                            className='max-w-xl p-4 mx-auto text-center text-white rounded-2xl'
+                        >
+                            Soon to be Announced
+                        </h1>
+
+                        {/* Coming Soon */}
+                        {/* <div className='relative px-10 py-20 space-x-3 sm:py-28 md:py-40 md:space-y-5 rounded-2xl bg-beige lg:mt-52'>
                             <div className='absolute top-0 left-0'>
                                 <figure className='overflow-hidden transform -translate-y-5 translate-x-11 md:-translate-y-10 lg:-translate-y-36 W-1/2 w-72 md:w-72 lg:w-96 xl:w-120 rounded-2xl'>
                                     <Image
@@ -128,7 +118,6 @@ export default function ibl_case_challenge() {
                                             width: 14rem;
                                         }
                                     }
-
                                     @media (min-width: 370px) and (max-width: 400px) {
                                         div,
                                         figure {
@@ -163,11 +152,34 @@ export default function ibl_case_challenge() {
                                     Register for IBL Case Challenge
                                 </ButtonLink>
                             </div>
+                        </div> */}
+                    </article>
+                </section>
+                {/* prize section */}
+                <section
+                    className='bg-watusi'
+                    style={{ background: 'rgba(234, 251, 240, 1)' }}
+                >
+                    <article className='min-h-screen py-16 space-y-8 layout'>
+                        <h2 className='text-center mb-14'>Total Prize</h2>
+                        <div
+                            className='p-8 shadow-lg text-watusi md:p-16 md:x-16 md:py-20 rounded-xl'
+                            style={{ background: 'rgba(238, 252, 242, 1)' }}
+                        >
+                            <div className='grid grid-cols-3 gap-x-8'>
+                                <figure>
+                                    <Image
+                                        src='/prize-1.svg'
+                                        height={300}
+                                        width={300}
+                                    />
+                                </figure>
+                            </div>
                         </div>
                     </article>
                 </section>
             </main>
             <Footer />
-        </>
+        </div>
     );
 }
