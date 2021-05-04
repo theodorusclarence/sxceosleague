ae6247e012f236dad23e18fb872e316348b4a7c0
Theodorus Clarence <theodorusclarence@gmail.com>
2021-05-04 22:34:13 +0700

    feat(ibl-case): add timeline

---
+++ data/ibl-case.js
@@ -0,0 +1,35 @@
+export const iblCaseTimeline = [
+    {
+        eventDate: '4 June',
+        eventName: 'Open Registration of IBL Case Challenge',
+    },
+    {
+        eventDate: '30 June',
+        eventName: 'Close Registration of IBL Case Challenge',
+    },
+    { eventDate: '2 July', eventName: 'Case Release' },
+    {
+        eventDate: '4 July',
+        eventName: 'Coaching Session #1 & Technical Meeting for First Stage. ',
+    },
+    { eventDate: '18 July', eventName: 'Paper Submission' },
+    { eventDate: '23 July', eventName: 'First Stage Announcement' },
+    {
+        eventDate: '25 July',
+        eventName: 'Coaching Session #2 & Technical Meeting for Second Stage.',
+    },
+    {
+        eventDate: '30 July',
+        eventName: 'Second Stage : Debating & O-QnA (Objective Q&A)',
+    },
+    { eventDate: '6 August', eventName: 'Finalist Announcement ' },
+    {
+        eventDate: '8 August',
+        eventName: 'Coaching Session #3 & Technical Meeting for Finalists. ',
+    },
+    {
+        eventDate: '14 August',
+        eventName: 'Finalist Pitching Presentation ',
+    },
+    { eventDate: '17 August', eventName: 'Awarding Night' },
+];
