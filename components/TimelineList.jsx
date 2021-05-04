364ef1bd346cf9032a94c8fd3d9fc0fcd4360ff9
Theodorus Clarence <theodorusclarence@gmail.com>
2021-05-04 21:29:08 +0700

    style: remove comment

--- components/TimelineList.jsx
+++ components/TimelineList.jsx
@@ -24,22 +24,5 @@ export default function TimelineList({ eventDate, eventName }) {
                 </div>
             </div>
         </div>
-        // <div className='p-4 text-tundora lg:x-16'>
-        //     <div className='flex flex-col mx-2 text-center md:justify-center md:flex-row md:space-x-8 lg:space-x-24 md:items-center'>
-        //         <div className='p-1 rounded-tl-full rounded-br-full bg-gradient-to-r from-geraldine to-goldentainoi'>
-        //             <div className='text-white rounded-tl-full rounded-br-full bg-gradient-to-r from-geraldine to-goldentainoi md:px-9 lg:py-3'>
-        //                 <p className='my-0 md:text-xl lg:mx-0 lg:text-2xl'>
-        //                     {eventDate}
-        //                 </p>
-        //             </div>
-        //         </div>
-        //         <img
-        //             src='/images/green-circle.svg'
-        //             alt='green-circle'
-        //             className='w-10 h-10 mx-auto my-3 md:w-16'
-        //         />
-        //         <p className='md:text-2xl'>{eventName}</p>
-        //     </div>
-        // </div>
     );
 }
