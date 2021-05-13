37279e4e474f2e376b47311f728286dd062abd8a
Theodorus Clarence <theodorusclarence@gmail.com>
2021-05-13 16:21:23 +0700

    feat(pre-event): add mini challenge close notice

---
+++ pages/r/mini-challenge.jsx
@@ -0,0 +1,7 @@
+import ClosedPage from '@/components/ClosedPage';
+
+export default function MiniChallengeClosed() {
+    return (
+        <ClosedPage title='Mini Challenge Registration is Officially Closed' />
+    );
+}
