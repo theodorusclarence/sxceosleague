4c116510117f87e777adaa973ba4de040203ac7e
Theodorus Clarence <theodorusclarence@gmail.com>
2021-05-13 16:24:41 +0700

    feat: add title to mini-challenge registration

--- pages/r/mini-challenge.jsx
+++ pages/r/mini-challenge.jsx
@@ -1,7 +1,12 @@
+import { NextSeo } from 'next-seo';
+
 import ClosedPage from '@/components/ClosedPage';
 
 export default function MiniChallengeClosed() {
     return (
-        <ClosedPage title='Mini Challenge Registration is Officially Closed' />
+        <>
+            <NextSeo title='Registration Closed - StudentsxCEOs League' />
+            <ClosedPage title='Mini Challenge Registration is Officially Closed' />
+        </>
     );
 }
