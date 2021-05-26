1afc768c8f7486439130ef2611c9be58dd47897f
Theodorus Clarence <theodorusclarence@gmail.com>
2021-05-26 14:13:57 +0700

    feat: add navbar on 404

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,13 @@
 import { NextSeo } from 'next-seo';
 
 import ClosedPage from '@/components/ClosedPage';
+import Nav from '@/components/Nav';
 
 export default function NotFoundPage() {
     return (
         <>
             <NextSeo title='Page Not Found - StudentsxCEOs League' />
+            <Nav />
             <ClosedPage title='Page Not Found' />
         </>
     );
