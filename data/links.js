79e1c4ad792ec7989d532aaaeb7c8dfc65ba5248
Theodorus Clarence <theodorusclarence@gmail.com>
2021-05-26 14:15:05 +0700

    feat: add conference registration

--- data/links.js
+++ data/links.js
@@ -1,5 +1,9 @@
 export const links = [
     {
+        label: 'Register Conference',
+        url: '/conference',
+    },
+    {
         label: 'Tiktok',
         url: 'https://vt.tiktok.com/ZSJhaoYaU/',
     },
