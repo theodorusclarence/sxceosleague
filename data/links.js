dab7d156dd0da7b6298e8acb48977269f6d4a520
reyhannaufal <reyhannaufal21@gmail.com>
2021-07-11 01:51:23 +0800

    feat(conference): close conference registration

--- data/links.js
+++ data/links.js
@@ -1,9 +1,5 @@
 export const links = [
     {
-        label: 'Register Conference',
-        url: '/conference',
-    },
-    {
         label: 'Visit SxCLeague Shop',
         url: 'https://instagram.com/sxcleague_shop',
     },
